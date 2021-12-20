55bc4a5c03758e24a3e82b2de3157dd0bf042ec7
Jana Semenova <jana.semenova@yolawo.de>
2021-12-20 21:03:01 +0100

    added routerLink

--- src/app/courses/courses-routing.module.ts
+++ src/app/courses/courses-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { CourseComponent } from "./course/course.component";
 import { HomeComponent } from "./home/home.component";
 
 const routes: Routes = [
   {
     path: "",
     component: HomeComponent,
+  },
+  {
+    path: ":courseUrl",
+    component: CourseComponent,
   },
 ];
 
