2cb062ff0170de0e547c6c85356d0a6252127ac8
Jana Semenova <jana.semenova@yolawo.de>
2021-12-21 12:48:25 +0100

    Router Guards

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,49 +1,40 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
-
-import {Router} from '@angular/router';
-import {AuthStore} from '../services/auth.store';
+import { Router } from "@angular/router";
+import { AuthStore } from "../services/auth.store";
 
 @Component({
-  selector: 'login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent implements OnInit {
-
   form: FormGroup;
 
   constructor(
     private fb: FormBuilder,
     private router: Router,
-    private auth: AuthStore) {
-
+    private auth: AuthStore
+  ) {
     this.form = fb.group({
-      email: ['test@angular-university.io', [Validators.required]],
-      password: ['test', [Validators.required]]
+      email: ["test@angular-university.io", [Validators.required]],
+      password: ["test", [Validators.required]],
     });
-
   }
 
-  ngOnInit() {
-
-  }
+  ngOnInit() {}
 
   login() {
-
     const val = this.form.value;
 
-    this.auth.login(val.email, val.password)
-        .subscribe(
-            () => {},
-            err => {
-                alert("Login failed!");
-            }
-        );
-
-
-
+    this.auth.login(val.email, val.password).subscribe(
+      () => {
+        this.router.navigateByUrl("/courses");
+      },
+      (err) => {
+        alert("Login failed!");
+      }
+    );
   }
-
 }
